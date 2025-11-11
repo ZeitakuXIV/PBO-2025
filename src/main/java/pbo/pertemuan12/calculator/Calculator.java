e4c76570387a54c728952c4d87365cbb9c854753
ZeitakuXIV <nurrizalzid@gmail.com>
2025-11-11 16:58:49 +0700

    init

---
+++ src/main/java/pbo/pertemuan12/calculator/Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pbo.pertemuan12.calculator;
+
+/**
+ *
+ * @author nurri
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
